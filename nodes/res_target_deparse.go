18160caf199d8e3f96169e80161e633299efdf07
Thibaut Colar <tcolar@colar.net>
2020-03-21 14:33:30 -0700

    Deparse res target

--- nodes/res_target_deparse.go
+++ nodes/res_target_deparse.go
@@ -1,7 +1,31 @@
-// Auto-generated - DO NOT EDIT
+package pg_query_nodes
 
-package pg_query
+import (
+	"log"
 
-func (node ResTarget) Deparse(ctx DeparseContext) string {
-	panic("Not Implemented")
+	"github.com/lfittl/pg_query_go/deparse"
+)
+
+func (node ResTarget) Deparse(ctx deparse.Context) string {
+	if ctx == nil && node.Name != nil {
+		return *node.Name
+	}
+	var val string
+	switch ctx.Kind() {
+	case deparse.SelectContext:
+		val = node.Val.Deparse(nil)
+		return deparse.CompactJoin(
+			" AS ",
+			&val,
+			deparse.DeparseString(node.Name, false),
+		)
+	case deparse.UpdateContext:
+		val = node.Val.Deparse(nil)ts
+		return deparse.CompactJoin(
+			" = ",
+			&val,
+		)
+	}
+	log.Fatalf("Can't deparse %v in context %v", node, ctx)
+	return ""
 }
