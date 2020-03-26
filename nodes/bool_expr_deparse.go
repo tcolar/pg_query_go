2ad62f9373029c96e43f1383836aa4c0e4f18ecc
Thibaut Colar <tcolar@colar.net>
2020-03-25 20:24:45 -0700

    Implemented bool expr deparse

--- nodes/bool_expr_deparse.go
+++ nodes/bool_expr_deparse.go
@@ -7,5 +7,51 @@ import (
 )
 
 func (node BoolExpr) Deparse(ctx deparse.Context) string {
-	return fmt.Sprintf("NOT %s", node.Args.Items[0].Deparse(nil))
+	switch node.Boolop {
+	case AND_EXPR:
+		return node.deparseBoolExprAnd(ctx)
+	case OR_EXPR:
+		return node.deparseBoolExprOr(ctx)
+	default:
+		panic(fmt.Sprintf("Unexpected boolop : %v", node.Boolop))
+		//return fmt.Sprintf("NOT %s", node.Args.Items[0].Deparse(nil))
+	}
 }
+
+func (node BoolExpr) deparseBoolExprAnd(ctx deparse.Context) string {
+	return node.Args.DeparseSep(nil, " AND ")
+	/*for _, arg := range node.Args.Items {
+		arg.Deparse
+	}*/
+}
+
+func (node BoolExpr) deparseBoolExprOr(ctx deparse.Context) string {
+	return node.Args.DeparseSep(nil, " OR ")
+	/*for _, arg := range node.Args.Items {
+		arg.Deparse
+	}*/
+}
+
+/*
+def deparse_bool_expr_and(node)
+# Only put parantheses around OR nodes that are inside this one
+node['args'].map do |arg|
+  if [BOOL_EXPR_OR].include?(arg.values[0]['boolop'])
+	format('(%s)', deparse_item(arg))
+  else
+	deparse_item(arg)
+  end
+end.join(' AND ')
+end
+
+def deparse_bool_expr_or(node)
+# Put parantheses around AND + OR nodes that are inside
+node['args'].map do |arg|
+  if [BOOL_EXPR_AND, BOOL_EXPR_OR].include?(arg.values[0]['boolop'])
+	format('(%s)', deparse_item(arg))
+  else
+	deparse_item(arg)
+  end
+end.join(' OR ')
+end
+*/
--- nodes/string_deparse.go
+++ nodes/string_deparse.go
@@ -1,5 +1,3 @@
-// Auto-generated - DO NOT EDIT
-
 package pg_query_nodes
 
 import (
