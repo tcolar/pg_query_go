f7511ea029615b9696ae6a3e6a3e746f37e91d5b
Thibaut Colar <tcolar@colar.net>
2020-03-25 20:25:06 -0700

    Fix indirection deparse

--- nodes/a_indirection_deparse.go
+++ nodes/a_indirection_deparse.go
@@ -1,28 +1,22 @@
 package pg_query_nodes
 
-import "github.com/lfittl/pg_query_go/deparse"
+import (
+	"fmt"
+
+	"github.com/lfittl/pg_query_go/deparse"
+)
 
 func (node A_Indirection) Deparse(ctx deparse.Context) string {
 
 	panic("Not Implemented")
 
 	o := deparse.Output{}
-	o.Append(node.Arg.Deparse(nil))
-	// How to do the ?key thing ??
+	switch node.Arg.(type) {
+	case FuncCall, SubLink:
+		o.Append(fmt.Sprintf("(%s)", node.Arg.Deparse((nil))))
+	default:
+		o.Append(node.Arg.Deparse(nil))
+	}
 	o.Append(node.Indirection.Deparse(nil))
 	return o.String()
 }
-
-/*
-      output = []
-      arg = deparse_item(node['arg'])
-      output << if node['arg'].key?(FUNC_CALL) || node['arg'].key?(SUB_LINK)
-                  "(#{arg})."
-                else
-                  arg
-                end
-      node['indirection'].each do |subnode|
-        output << deparse_item(subnode)
-      end
-	  output.join
-*/
